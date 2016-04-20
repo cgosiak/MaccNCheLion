f6125accb3259208e1441152020cabb729ef8553
Caleb J. Gosiak <clubs67charger@gmail.com>
2016-04-19 23:02:25 -0500

    String limit issue

--- test.asm
+++ test.asm
@@ -7,9 +7,32 @@ WRNL
 RDST      L4T2
 WRST      L4T2
 WRNL      
+WRST      L11T0
+WRNL      
+RDI       L1T0
+RDI       L2T0
+RDI       L3T0
+WRI       L1T0
+WRST      L11T1
+WRI       L2T0
+WRST      L11T2
+WRI       L3T0
+WRNL      
 HALT      
 % Data Area ------------
 % ----------------------
+LABEL    LBL1
+INT   0
+LABEL    L1T0
+INT       0
+LABEL    LBL2
+INT   0
+LABEL    L2T0
+INT       0
+LABEL    LBL3
+INT   0
+LABEL    L3T0
+INT       0
 LABEL    LBL4
 STRING   ""
 LABEL    L4T0
@@ -18,6 +41,14 @@ LABEL    L4T1
 STRING    ""
 LABEL    L4T2
 STRING    ""
+LABEL    LBL11
+STRING   ""
+LABEL    L11T0
+STRING    "Type 3 Integers (Each separated by a newline)::"
+LABEL    L11T1
+STRING    " "
+LABEL    L11T2
+STRING    " "
 
 LABEL     VARS
 SKIP      2
